a2c09d8065be91e431f13e2eb23c327d0f63e705
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 23:42:35 +0000

    Mockups (not finished)

--- app/src/main/java/ipvc/estg/trabalhofinal/MainActivity.kt
+++ app/src/main/java/ipvc/estg/trabalhofinal/MainActivity.kt
@@ -1,9 +1,12 @@
 package ipvc.estg.trabalhofinal
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.RelativeLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -20,6 +23,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
 
     private var scanning: Boolean = false //Variavel de estado do scanner
 
+    private var popupInfo: PopupWindow? = null //popup da informaçao
+
     private lateinit var directionsPoint: LatLng  //Variavel do ponto das direçoes, definido quando o utilizador clica no mapa
 
 
@@ -33,7 +38,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
     }
 
     /*Quando se carrega no beacon scanner*/
-    fun onBeaconScannerClick(v: View)  {
+    fun onBeaconScannerClick(v: View?)  {
         scanning = !scanning
 
         val fab: FloatingActionButton = findViewById(R.id.fab_beacon_scanner)
@@ -41,10 +46,47 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
         if(scanning){
             Toast.makeText(this,"${getString(R.string.searching)}...",Toast.LENGTH_SHORT).show()
             fab.setColorFilter(ContextCompat.getColor(this, R.color.red_200), android.graphics.PorterDuff.Mode.SRC_IN)
+            showInfoPopup()
         }else{
+            dismissPopup()
             fab.setColorFilter(ContextCompat.getColor(this, R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
         }
     }
+
+    fun dismissPopup(){
+        if(popupInfo != null){
+            popupInfo!!.dismiss()
+            popupInfo = null
+        }
+    }
+
+    fun showInfoPopup(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popup = inflater.inflate(R.layout.popup_info, null)
+
+        val width: Int = LinearLayout.LayoutParams.MATCH_PARENT
+        val height: Int = LinearLayout.LayoutParams.WRAP_CONTENT
+        val focusable = false
+
+        val popupWindow = PopupWindow(popup, width, height, focusable)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.elevation = 10F
+        }
+
+        popupWindow.showAtLocation(popup, Gravity.TOP, 20, 0)
+
+        popupInfo = popupWindow //associa o popupwindow local ao popupinfo global
+
+        popup.findViewById<Button>(R.id.cancelInfoPopupButton).setOnClickListener {
+            onBeaconScannerClick(null)
+            dismissPopup()
+        }
+
+        popup.findViewById<Button>(R.id.goToInfoButton).setOnClickListener {
+            Toast.makeText(this,"Going...",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     /*Listener aos clicks no mapa*/
     override fun onMapClick(point: LatLng) {
@@ -56,13 +98,13 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
     }
 
     /*Botão de cancelar as direçoes*/
-    fun cancelDirections(v: View){
+    fun cancelDirections(v: View?){
         mMap.clear()
         findViewById<RelativeLayout>(R.id.directions).visibility = View.GONE
     }
 
     /*Botão de calcular direções*/
-    fun goToDirections(v: View){
+    fun goToDirections(v: View?){
         Toast.makeText(this,"Calculando direções",Toast.LENGTH_SHORT).show()
     }
 
